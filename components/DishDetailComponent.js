83a8a053dfc3fc0d82a37aeb48c27a3b881f2ca1
furkan-cloud <firstinfirstout2020@gmail.com>
2020-11-02 02:30:45 +0300

    add favorite feat to dish component

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Text, View, ScrollView, FlatList} from 'react-native';
-import {Card} from 'react-native-elements';
+import {Card, Icon} from 'react-native-elements';
 import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
 
@@ -15,6 +15,16 @@ function RenderDish(props) {
         {/* <Card.FeaturedTitle>{dish.name}</Card.FeaturedTitle> */}
         {/* <Card.Image source={require('./images/uthappizza.png')} /> */}
         <Text style={{margin: 10}}>{dish.description}</Text>
+        <Icon
+          raised
+          reverse
+          name={props.favorite ? 'heart' : 'heart-o'}
+          type="font-awesome"
+          color="#f50"
+          onPress={() =>
+            props.favorite ? console.log('Already favorite') : props.onPress()
+          }
+        />
       </Card>
     );
   } else {
@@ -50,11 +60,16 @@ const RenderComments = (props) => {
 function Dishdetail(props) {
   const [dishes, setDishes] = useState(DISHES);
   const [comments, setComments] = useState(COMMENTS);
+  const [favorites, setFavorites] = useState([]);
 
   const {dishId} = props.route.params;
   return (
     <ScrollView>
-      <RenderDish dish={dishes[dishId]} />
+      <RenderDish
+        dish={dishes[dishId]}
+        favorite={favorites.some((el) => el === dishId)}
+        onPress={() => setFavorites(favorites.concat(dishId))}
+      />
       <RenderComments
         comments={comments.filter((comment) => dishId === comment.dishId)}
       />
