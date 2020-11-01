8dc6c7d4b10895b1db8cabff14f9db7095899cf8
furkan-cloud <firstinfirstout2020@gmail.com>
2020-11-02 01:58:07 +0300

    comments added to dishdetail component

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView, FlatList} from 'react-native';
 import {Card} from 'react-native-elements';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
 
 function RenderDish(props) {
   const dish = props.dish;
@@ -21,11 +22,44 @@ function RenderDish(props) {
   }
 }
 
+const RenderComments = (props) => {
+  const comments = props.comments;
+
+  const renderCommentItem = ({item, index}) => {
+    return (
+      <View key={index} style={{margin: 10}}>
+        <Text style={{fontSize: 14}}>{item.comment}</Text>
+        <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+        <Text style={{fontSize: 12}}>
+          {'-- ' + item.author + ', ' + item.date}
+        </Text>
+      </View>
+    );
+  };
+  return (
+    <Card title="Comments">
+      <FlatList
+        renderItem={renderCommentItem}
+        data={comments}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </Card>
+  );
+};
+
 function Dishdetail(props) {
   const [dishes, setDishes] = useState(DISHES);
+  const [comments, setComments] = useState(COMMENTS);
+
   const {dishId} = props.route.params;
-    console.warn(dishId)
-  return <RenderDish dish={dishes[dishId]} />;
+  return (
+    <ScrollView>
+      <RenderDish dish={dishes[dishId]} />
+      <RenderComments
+        comments={comments.filter((comment) => dishId === comment.dishId)}
+      />
+    </ScrollView>
+  );
 }
 
 export default Dishdetail;
